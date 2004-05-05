cf83b8153599b28f9799d2e64c7b882a83c8ad0b
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-05-05 16:20:31 +0000

    added vignettes to win pulldown
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affycomp@8510 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,16 @@
 .First.lib <- function(libname, pkgname, where) {
-  
+
 
   where <- match(paste("package:", pkgname, sep=""), search())
 
   require(Biobase, quietly=TRUE) ##Biobase uses methods
+
+    if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
+        && .Platform$GUI ==  "Rgui"){
+        addPDF2Vig("affycomp")
+    }
+
+
 
   cacheMetaData(as.environment(where))
 
